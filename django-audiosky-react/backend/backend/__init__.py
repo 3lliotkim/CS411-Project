d5c69f297547f6de99308cb2848836bc7654449c
SanjanaK1 <skasarla@bu.edu>
2022-12-06 14:15:50 -0500

    Changes to Frontend, Weather API
    Added CSS files, a new weather page to view the daily weather and updated the api to include useful information

--- django-audiosky-react/backend/backend/__init__.py
+++ django-audiosky-react/backend/backend/__init__.py
@@ -1,12 +1,27 @@
 import requests
 
-url = "http://api.weatherapi.com/v1/current.json?key=620989d8befc4782ac2172558221111&q=Boston&aqi=no"
+url = "http://api.weatherapi.com/v1/current.json?key=27c7234523dc4d33bf3185545220612&q=Boston&aqi=no"
+
+hot = False
 
 response = requests.get(url)
 json = response.json()
-temperature = json["current"]["temp_f"]
-feelslike = json["current"]["feelslike_f"]
-windSpeed = json["current"]["wind_mph"]
-print("Todays Temperature:",temperature)
-print("Todays feels like:",feelslike)
-print("Todays Wind Speed:",windSpeed,"mph")
+temperature = json["current"]["temp_f"]  #see if it is hot or cold
+windSpeed = json["current"]["wind_mph"] #high wind speed = cold
+cloud = json["current"]["cloud"]             #cloudy 
+precipitation = json["current"]["precip_in"] #rainy
+sunny = json["current"]["uv"]                #sunny 
+
+
+if(sunny >= 6):
+    hot == True
+
+print("Temperature:",temperature)
+print("Wind Speed:",windSpeed,"mph")
+print("Percentage of Cloudiness:",cloud)
+print("Rain in inches :",precipitation)
+if hot == True:
+    print("UV",sunny, "Today is sunny")
+else:
+    print("UV",sunny, "Today is not very sunny")
+
