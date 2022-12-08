080de73dc9002c988d812c18650578eac1f62dc5
Fabian <113716294+Fanzures@users.noreply.github.com>
2022-12-07 21:00:52 -0500

    displays table but needs conditional logic

--- AudioSky/web-api-auth-exmples/authorization_code/public/__init__.py
+++ AudioSky/web-api-auth-exmples/authorization_code/public/__init__.py
@@ -1,8 +1,10 @@
 import requests
-url = "WEATHER API URL ACCESS"
+from generate_playlsit import get_playlist
+from IPython.display import HTML
+url = "WEATHER API URL"
 
 hot = False
-
+playlist_displayed= ""
 response = requests.get(url)
 json = response.json()
 temperature = str(json["current"]["temp_f"])  #see if it is hot or cold
@@ -14,29 +16,13 @@ sunny = str(json["current"]["uv"])                #sunny
 
 if(float(sunny) >= 6):
     hot == True
-
-temp_word = "Temperature:"
-wind_word = "Wind Speed:"
-cloud_word = "Percentage of Cloudiness:"
-rain_word = "Rain in inches :"
-
-
-#temp_txt = temp_word + temperature
-#wind_txt = wind_word + windSpeed + "mph"
-#cloud_txt = cloud_word + cloud
-#rain_txt = rain_word + percipitation
-
-
+      
 if hot == True:
-    sunny_txt = "UV" + sunny + "Today is sunny"
-else:
     sunny_txt = "UV" + sunny +  "Today is not very sunny"
-
-#temp_txt = "Todays temperature is {temp} ".format(temp = temperature)
-#wind_txt = "Wind Speed {wind} ".format(wind = windSpeed)
-#cloud_txt = "Percentage of Cloudiness {cl} ".format(cl = cloud)
-#rain_txt = "Rain in Inches {rain} ".format(rain = precipitation)
-
+else:
+    playlist_displayed =get_playlist('spotify', '37i9dQZF1DWSqBruwoIXkA')
+   
+html_table = playlist_displayed.to_html()
 
 f = open(r"PATH ON YOUR PC", "w+")
 text = '''<!DOCTYPE html>
@@ -51,13 +37,16 @@ text = '''<!DOCTYPE html>
     <p>Wind Speed: {wind}</p>
     <p>Percentage of Cloudiness: {cl}</p>
     <p>Rain in Inches: {rain}</p>
+
+    <p> How about these songs for today? </p>
+    {playlist} 
     <button>
         <a type="button" class="b1" href="https://www.spotify.com">Get My Generated Playlist</a>
     </button>
     
 </body>
 </html>
-'''.format(temp = temperature, wind = windSpeed, cl = cloud, rain = percipitation)
+'''.format(temp = temperature, wind = windSpeed, cl = cloud, rain = percipitation, playlist = html_table)
 f.write(text)
 f.close()
 print(f)
