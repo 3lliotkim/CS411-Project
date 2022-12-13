fa57c6ec128faabf6b8f8360e55d4ab03e51d76a
Fabian <113716294+Fanzures@users.noreply.github.com>
2022-12-12 23:51:57 -0500

    Delete app.js

--- AudioSky/web-api-auth-exmples/implicit_grant/app.js
+++
@@ -1,5 +0,0 @@
-var express = require('express'); // Express web server framework
-var app = express();
-app.use(express.static(__dirname + '/public'));
-console.log('Listening on 8888');
-app.listen(8888);
